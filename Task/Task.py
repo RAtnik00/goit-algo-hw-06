784b6026a7767666596dfb153d8fd73375d97eec
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-07-28 16:14:36 +0200

    create class Phone

---
+++ Task/Task.py
@@ -0,0 +1,28 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+    
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be at least 10 digits long and contain only numbers.")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {';'.join(p.value for p in self.phones)}"
+    
+class AddressBook(UserDict):
+    pass
