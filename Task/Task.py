d64c08fae6d5c110ac3a0d0319a3cc1a3b73be36
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-07-29 14:32:21 +0200

    done

--- Task/Task.py
+++ Task/Task.py
@@ -29,17 +29,18 @@ class Record:
         self.phones.append(new_phone)
 
     def remove_phone(self, phone: str):
-        phone_obj = self.find_phone(phone)
-        if phone_obj:
-            self.phones.remove(phone_obj)
-        else:
-            raise ValueError("Phone number not found.")
-
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                return f"Phone {phone} removed."
+        return f"Phone {phone} not found."
     
     def edit_phone(self, old_phone: str, new_phone: str):
-        self.remove_phone(old_phone)  
-        self.add_phone(new_phone)    
-
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                return f"Phone {old_phone} changed to {new_phone}."
+        return f"Phone {old_phone} not found."
     
     def find_phone(self, phone: str):
         for p in self.phones:
@@ -57,9 +58,8 @@ class AddressBook(UserDict):
     def delete(self, name: str):
         if name in self.data:
             del self.data[name]
-        else:
-            raise KeyError("Contact not found.")
-
+            return f"Contact {name} deleted."
+        return f"Contact {name} not found."
     
     def __str__(self):
         return "\n".join(str(record) for record in self.data.values())
